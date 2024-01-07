dccad8507e3c9504f6a2dc901435e982c5304466
vikash-frontier <vikash@frontier.xyz>
2024-01-07 16:59:09 +0530

    Upadated some style

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -13,12 +13,12 @@ const CardItem = () => {
         {MARKETING_DATA.map((item) => (
           <motion.div
             key={item.id}
-            className="card-item  w-40 h-40 md:w-[200px] md:h-[200px] lg:w-60 lg:h-60  rounded-[20px] flex justify-center items-center flex-col relative "
+            className="card-item  w-[200px] h-[200px] lg:w-60 lg:h-60  rounded-[20px] flex justify-center items-center flex-col relative "
             initial={{ opacity: 0, y: 80 }}
             animate={control}
             transition={{ type: "tween", duration: 0.7, delay: 0.2 }}
           >
-            <div className="card-price-bg py-1 paragraph text-[16px] absolute  right-5 md:right-8 top-2 w-16 text-center md:top-4">
+            <div className="card-price-bg py-1 paragraph text-[16px] absolute  right-5 md:right-8 top-4 md:top-2 w-16 text-center lg:top-4">
               <span>11.2K</span>
             </div>
             <img
@@ -26,7 +26,7 @@ const CardItem = () => {
               alt={`${item.name}`}
               className="h-20 w-24 md:h-32 mt-5 md:w-40"
             />
-            <p className="paragraph md:mt-4 text-[20px]  md:text-3xl ">
+            <p className="paragraph lg:mt-4 text-[20px]  lg:text-3xl ">
               {item.name}
             </p>
           </motion.div>
